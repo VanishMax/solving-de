677df2d4ae04361614c21b02de9900fd4a39eec1
VanishMax <maxkors2014@gmail.com>
2019-10-23 10:39:45 +0300

    Write equations of all variants

--- config.js
+++ config.js
@@ -59,8 +59,31 @@ const options = {
 };
 
 const variants = {
+  1: '1 + 2*(y/x)',
+  2: 'y/x - x * Math.exp(y/x)',
+  3: '1/Math.cos(x) - y * Math.tan(x)',
+  4: '2 * Math.pow(x, 3) + 2 * (y/x)',
+  5: 'y/x + x * Math.cos(x)',
+  6: '(2 * x) / (x*x + y)',
+  7: '1/x + (2 * y) / (x * Math.log(x))',
+  8: 'y*y * Math.exp(x) - 2 * y',
+  9: '4 / (x*x) - y/x - y*y',
+  10: '-((y*y) / 3) - 2/(3 * x*x)',
   11: '(2*x+1)*(y/x) - (y*y)/x - x',
+  12: '5 - x*x - y*y +2 * x * y',
+  13: 'Math.exp(2*x) + Math.exp(x) + y*y - 2 * y * Math.exp(x)',
   14: '(1 + y/x) * Math.log((x + y)/x) + y/x',
+  15: '1/(x + 1) + 1/y',
+  16: 'Math.exp(y) − 2/x',
+  17: '3 * Math.pow(y, 2/3)',
+  18: '(y*y - y) / x',
+  19: '2 * x + y - 3',
+  20: '(2 - y*y) / (2 * x*x * y)',
+  21: 'y/x - y - x',
+  22: '(3 * y + 2 * x * y) / (x*x)',
+  23: 'Math.pow(y - x, 1/2) / Math.pow(x, 1/2) + 1',
+  24: '2 * Math.cos(x) / Math.pow(x, 1/2) - 2 * (y/x)',
+  25: '3*y - x * Math.pow(y, 1/3)',
 };
 
 const randColor = () => {
